f5912be613be528a8484c4a06b2c95f48b68c4b5
James Daniels <31466053+jdani007@users.noreply.github.com>
2024-04-02 13:32:07 -0500

    Add files via upload

---
+++ main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	nums := []int{7, 4, 5, 3, 1, 2, 6, 0}
+	counter := 0
+	for i := len(nums); i > 0; i-- {
+		for j :=0; j <  i; j++ {
+			if nums[j] > nums[i-1] {
+				nums[i-1], nums[j] = nums[j], nums[i-1]
+				counter++
+			}
+		}
+	}
+	fmt.Println(nums)
+}
